77453e882d564a2d3fea8ffe9a578b38c4b98048
binod <binod.batech@gmail.com>
2020-01-16 19:33:04 -0500

    commit for logger

--- TestProject/src/main/java/com/ben/poc/TestProjectApplication.java
+++ TestProject/src/main/java/com/ben/poc/TestProjectApplication.java
@@ -1,14 +1,17 @@
 package com.ben.poc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestProjectApplication {
-
+	public static Logger logger= LoggerFactory.getLogger(TestProjectApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TestProjectApplication.class, args);
-		
+		logger.info("testing application for jenkins");
 	}
 
 }
